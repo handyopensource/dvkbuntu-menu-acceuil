659b77fa458bc7e28e0f5f1452a8b55a4282ae09
Paul WOISARD <paulwoisard@gmail.com>
2020-02-16 16:25:01 +0100

    Ajout icone et correction faute de frappe

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling); // DPI support
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps); //HiDPI pixmaps
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/images/Logo.png"));
     MainWindow w;
     w.show();
 
